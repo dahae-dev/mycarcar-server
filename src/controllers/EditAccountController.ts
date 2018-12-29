599eefbc956b232cfe1c79bea965c7a7ef840315
inceptionJh <kjaehun6546@gmail.com>
2018-12-29 15:58:41 +0900

    Refactor auth handling

--- src/controllers/EditAccountController.ts
+++ src/controllers/EditAccountController.ts
@@ -1,44 +1,30 @@
 import { Request, Response } from "express";
 import UserModel from "../models/user/UserModel";
+import JwtManager from "../util/JwtManager";
 
 export default class EditAccountController {
   public async getUserInfomation(req: Request, res: Response): Promise<void> {
     const userModel = new UserModel();
-    const id = req.body.id;
+    const jwtManager = new JwtManager(req);
 
-    const userInfomations = await userModel.getUser({ id });
-    const hasNotUserInfomation =
-      userInfomations[0] === undefined ? true : false;
+    const idOfRequester = jwtManager.getDecodedToken().id;
 
-    if (hasNotUserInfomation) {
-      res.statusCode = 412;
-      res.statusMessage = "[-] Member information does not exist.";
-      return res.end();
-    }
+    const userInfomation = (await userModel.getUser({ id: idOfRequester }))[0];
 
     res.statusCode = 200;
     res.statusMessage = "[+] Member information has been returned as normal.";
-    res.json(userInfomations[0]);
+    res.json(userInfomation);
   }
 
-  public async updateAcountInfomation(
+  public async updateAccountInfomation(
     req: Request,
     res: Response,
   ): Promise<void> {
     const userModel = new UserModel();
+
     const { name, id, pw, email, phone } = req.body as IChangedRegisterInfo;
 
-    const userInfomations = await userModel.getUser({ id });
-    const hasNotUserInfomation =
-      userInfomations[0] === undefined ? true : false;
-
-    if (hasNotUserInfomation) {
-      res.statusCode = 412;
-      res.statusMessage = "[-] Member information does not exist.";
-      return res.end();
-    }
-
-    userModel.updateUser({ name, id, pw, email, phone });
+    await userModel.updateUser({ name, id, pw, email, phone });
     res.statusCode = 200;
     res.statusMessage = "[+] Member information has been modified as normal..";
     res.end();
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,33 +1,31 @@
 import { Request, Response } from "express";
 import { NextFunction } from "connect";
 
-import jsonwebtoken from "jsonwebtoken";
+import JwtManager from "../util/JwtManager";
 
 export const authMiddlemare = (
   req: Request,
   res: Response,
   next: NextFunction,
 ): void => {
-  if (process.env.SECRET === undefined) {
-    throw new Error("[-] : .env file에 SECRET이 없습니다.");
-  }
+  const jwtManager = new JwtManager(req);
 
-  if (req.headers["x-access-token"] === undefined) {
+  const hasNotRawToken = !jwtManager.hasRawToken();
+  if (hasNotRawToken) {
     res.statusCode = 401;
     res.statusMessage = "[-] You don't have a token.";
     return res.end();
   }
 
-  if (Array.isArray(req.headers["x-access-token"])) {
+  const isNotInvalidRawToken = !jwtManager.isInvalidRawToken();
+  if (isNotInvalidRawToken) {
     res.statusCode = 401;
     res.statusMessage = "[-] Invalid token.";
     return res.end();
   }
 
-  const secret: string = process.env.SECRET;
-  const rawToken: string = req.headers["x-access-token"] as string;
-  const decodedToken = jsonwebtoken.verify(rawToken, secret) as IDecodedToken;
-  const isValidToken: boolean = decodedToken.exp > decodedToken.iat;
+  const decodedToken = jwtManager.getDecodedToken();
+  const isValidToken = jwtManager.isValidToken(decodedToken);
 
   if (isValidToken) {
     return next();
