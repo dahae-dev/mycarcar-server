ac5d58d10fdd4652715b107a943c8a47063a2f28
dahaekim88 <dhkim1014@gmail.com>
2019-01-03 17:19:06 +0900

    Modify static files for handling toggle sidebar

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8299359e55328ecca411750df97e0b86.js"
+  "/precache-manifest.447370ae965a1773256bf08cc280df86.js"
 );
 
 workbox.clientsClaim();
