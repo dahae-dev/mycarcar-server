907ca89c7b71d10ef3fdba5fa002695ddeb63ba7
dahaekim88 <dhkim1014@gmail.com>
2019-01-09 20:01:35 +0900

    Add query to retrieve car price data

--- src/models/car/RentalModel.ts
+++ src/models/car/RentalModel.ts
@@ -8,6 +8,7 @@ import {
   getSelectCarModelQuery,
   getSelectCarDetailQuery,
   getSelectCarGradeQuery,
+  gtSelectCarPriceQuery,
   getSelectCarOptionQuery
 } from "./RentalQuery";
 import {
@@ -16,30 +17,31 @@ import {
   ISelectCarModelList,
   ISelectCarDetailList,
   ISelectCarGradeList,
+  ISelectCarPrice,
   ISelectCarOptionList
 } from "../../_@types/Models/Car";
 import { sendQuery } from "../../db";
 
 /** 쿼리문 실행을 통해 차량 브랜드 정보 불러오기 */
-export const selectCarBrandList: ISelectCarBrandList = origin => {
+export const selectCarBrandList: ISelectCarBrandList = (origin) => {
   const query = getSelectCarBrandQuery(origin);
   return sendQuery(query);
 };
 
 /** 쿼리문 실행을 통해 차량 시리즈 정보 불러오기 */
-export const selectCarSeriesList: ISelectCarSeriesList = brand => {
+export const selectCarSeriesList: ISelectCarSeriesList = (brand) => {
   const query = getSelectCarSeriesQuery(brand);
   return sendQuery(query);
 };
 
 /** 쿼리문 실행을 통해 차량 모델 정보 불러오기 */
-export const selectCarModelList: ISelectCarModelList = series => {
+export const selectCarModelList: ISelectCarModelList = (series) => {
   const query = getSelectCarModelQuery(series);
   return sendQuery(query);
 };
 
 /** 쿼리문 실행을 통해 차량 상세모델 정보 불러오기 */
-export const selectCarDetailList: ISelectCarDetailList = model => {
+export const selectCarDetailList: ISelectCarDetailList = (model) => {
   const query = getSelectCarDetailQuery(model);
   return sendQuery(query);
 };
@@ -47,6 +49,12 @@ export const selectCarDetailList: ISelectCarDetailList = model => {
 /** 쿼리문 실행을 통해 차량 등급 정보 불러오기 */
 export const selectCarGradeList: ISelectCarGradeList = (model, detail) => {
   const query = getSelectCarGradeQuery(model, detail);
+  return sendQuery(query);
+};
+
+/** 쿼리문 실행을 통해 차량 가격 정보 불러오기 */
+export const selectCarPrice: ISelectCarPrice = (model, detail, grade) => {
+  const query = gtSelectCarPriceQuery(model, detail, grade);
   return sendQuery(query);
 };
 
