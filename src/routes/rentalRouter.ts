f75ce85a0714963e3ebd643c174baa0099ed312b
dahaekim88 <dhkim1014@gmail.com>
2019-01-09 13:19:20 +0900

    Implement rental api router

---
+++ src/routes/rentalRouter.ts
@@ -0,0 +1,24 @@
+/**
+ * 2주차 다해 - 차량 정보 요청을 위한 /api/rental 라우터
+ */
+
+import { Router } from "express";
+import {
+  getBrandListController,
+  getSeriesListController,
+  getModelListController,
+  getDetailListController,
+  getGradeListController,
+  getOptionListController
+} from "../controllers/rental/getCarData";
+
+const router = Router();
+
+router.get("/:origin", getBrandListController);
+router.get("/:origin/:brand", getSeriesListController);
+router.get("/:origin/:brand/:series", getModelListController);
+router.get("/:origin/:brand/:series/:model", getDetailListController);
+router.get("/:origin/:brand/:series/:model/:detail", getGradeListController);
+router.get("/:origin/:brand/:series/:model/:detail/:grade", getOptionListController);
+
+export default router;
