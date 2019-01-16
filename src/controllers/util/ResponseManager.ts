d4b8fae253280f4f81e4e2edfde87cdca255187e
inceptionJh <kjaehun6546@gmail.com>
2019-01-16 16:29:35 +0900

    Modify json of ResponseManager

--- src/controllers/util/ResponseManager.ts
+++ src/controllers/util/ResponseManager.ts
@@ -8,11 +8,6 @@ export default class ResponseManager {
   }
 
   json(statusCode: number, statusMessage: string, jsonData = {}) {
-    if (statusCode >= 200 && statusCode < 300) {
-      this.res.json({ statusCode, statusMessage, ...jsonData });
-      return;
-    }
-
     this.res.statusCode = statusCode;
     this.res.statusMessage = statusMessage;
     this.res.send(jsonData);
