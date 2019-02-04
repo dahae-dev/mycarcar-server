51ac82fadf4992644ad8574bbf6e06780c1be615
inceptionJh <kjaehun6546@gmail.com>
2019-02-04 21:07:45 +0900

    Update client

---
+++ build/precache-manifest.e11a888a61e800d3ee33d58743cbf2f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "afac29bd85ded41690c1",
+    "url": "/static/js/main.afac29bd.chunk.js"
+  },
+  {
+    "revision": "f5dca96fbf46069c6007",
+    "url": "/static/js/1.f5dca96f.chunk.js"
+  },
+  {
+    "revision": "afac29bd85ded41690c1",
+    "url": "/static/css/main.b8c2a9bc.chunk.css"
+  },
+  {
+    "revision": "07690745e171daa57596dc38a06630a7",
+    "url": "/index.html"
+  }
+];
